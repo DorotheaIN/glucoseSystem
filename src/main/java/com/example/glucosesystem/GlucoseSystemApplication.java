e737147a598f11515671365e8d175d412f5155fe
i527826 <dorothea.zhao@sap.com>
2023-04-27 15:21:01 +0800

    init

---
+++ src/main/java/com/example/glucosesystem/GlucoseSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.glucosesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlucoseSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlucoseSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/glucosesystem/GlucoseSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.glucosesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlucoseSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
